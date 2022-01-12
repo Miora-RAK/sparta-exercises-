9bf0eaa5a29de412a478a76a0e089d66b4529fe0
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-12 14:58:49 +0100

    add exercise object values and done

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/08_object_values/src/index.js
@@ -13,12 +13,18 @@ const odinRequest = "Mjöllnir";
 // using the object method to get all values (google is your friend)
 
 
-
+const armouryWithNamesOnly = [armoury.spear, armoury.hammer, armoury.chain, armoury.sword, armoury.shoes, armoury.horn];
 // Create a constant isTheRequestInArmoury bound to the check of the presence
 // of odinRequest in armouryWithNamesOnly
 
 
-
+const isTheRequestInArmoury = armouryWithNamesOnly.includes(odinRequest);
 // Create a condition that logs "Ah, I finally found something to hammer that nail into"
 // if the isTheRequestInArmoury is true and "What do you want me to do with this?" if not
 
+
+if (isTheRequestInArmoury) {
+  console.log("Ah, I finally found something to hammer that nail into");
+} else {
+  console.log("What do you want me to do with this?");
+}
