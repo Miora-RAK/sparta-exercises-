6390214caa8c6f4665cb1d3b553c8e30dc2a1ba4
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-12 11:57:27 +0100

    add exercise even and odd ternary and done

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/05_even_and_odd_numbers_ternary/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/05_even_and_odd_numbers_ternary/src/index.js
@@ -1,3 +1,6 @@
 let numberToCheck = 18;
 
 // transform (or better, re do) the code of your previous exercise using a ternary operator.
+
+
+(numberToCheck % 2) === 0 ? console.log("The number is even") : console.log("The number is odd");
