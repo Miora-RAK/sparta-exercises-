fe16c07c8c415018b1275ef1960f04b64ba81802
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-12 08:07:01 +0100

    exercise eleven and ten done

--- 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/10_play_with_push/src/index.js
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/10_play_with_push/src/index.js
@@ -1,5 +1,9 @@
 // Rewrite your array `digits` using `push` (it was like this: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 
+const digit = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const digits = digit.push(9);
 // create a constant called `last` for the last cell of your array, using length
 
+
+const last = digits.length - 1 ;
