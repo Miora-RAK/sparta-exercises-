93d4561fdbb62e80db332f7d0fb6543afb16b28b
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-13 11:52:15 +0100

    add and done exercise fortune teller

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -1,6 +1,7 @@
-function tellFortune() {
-  // Code the function here.
+function tellFortune( job, city, name, numberOfYears) {
+  console.log(`You will be ${job} in ${city}, and married to ${name} in ${numberOfYears} years.`);
 }
+
 
 // Do not remove last lines, it is for tests
 module.exports = tellFortune;
