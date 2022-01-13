48d23661fa6f56cb9eaa427200a7ad80b753c7c6
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-13 14:04:22 +0100

    add and done exercise odd even

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/03_odd_even/src/index.js
@@ -1,5 +1,13 @@
-function oddEven() {
-  // Code the function here.
+function oddEven(i) {
+  if (Number.isInteger(i)) {
+    if (i % 2 === 0) {
+      return `${i} is an even number.`;
+    } else if (i % 2 !== 0) {
+      return `${i} is an odd number.`;
+    }
+  } else {
+    console.log(`${i} is not a number.`);
+  }
 }
 
 // Do not remove last lines, it is for tests
