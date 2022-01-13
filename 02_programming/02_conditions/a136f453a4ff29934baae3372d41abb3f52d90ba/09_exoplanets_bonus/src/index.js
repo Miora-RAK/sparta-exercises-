6ba38c2d02dc0954437aa0a159481e341870d534
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-13 09:12:49 +0100

    add exercise bonus start

--- 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/09_exoplanets_bonus/src/index.js
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/09_exoplanets_bonus/src/index.js
@@ -1,9 +1,17 @@
 // Don't modify these lines for tests to work properly.
 const star = require("./data/starData");
 const planet = require("./data/exoplanetData");
+const { spectralClass } = require("./data/starData");
 
 // Your code here:
 let canHabitateLife;
+console.log(star);
+console.log(planet);
+
+canHabitateLife = (star && planet) ? true : false;
+if (spectralClass.star === 0, B, A, F, G){
+  canHabitateLife = true
+}
 
 // Don't modifiy this line for test to work properly.
 module.exports = canHabitateLife;
