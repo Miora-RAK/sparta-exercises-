cb13aff0e2c008dda10cb0f5c22fd7eafc5212f7
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-14 18:19:49 +0100

    exercise use callback modification

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -1,8 +1,13 @@
 // A function getInput that takes two parameters, an input (the first one) and a callback function
-const getInput = ({user, key},log) => {
-  const result = log("A");
+const getInput = (userInput,log) => {
+  const result = log(userInput.key);
   console.log (result);
 };
+/*
+function getIput (userInput,log){
+  log(userInput.key);
+}
+*/
 // code the function here
 // Leave the line below for tests to work
 module.exports = getInput;
