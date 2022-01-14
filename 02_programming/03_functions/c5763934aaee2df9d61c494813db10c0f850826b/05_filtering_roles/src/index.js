b69e67f46bb8274c9a985652d6aeb92fe7444902
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-14 10:23:09 +0100

    add exercise filtering not done

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,7 +1,27 @@
-function filteredRoles(){
-  // Code the function here.
+function filteredRoles(personList,role = "Teacher"){
+
+  console.log(personList);
+  console.log(role);
+  const studentList = personList.filter(personList => personList.role === "Student");
+  const teachersList = personList.filter(personList => personList.role === "Teacher");
+  const emptyArray = [{}];
+  if (role === "Teacher"){
+    console.log(emptyArray) ;
+  }else {
+    return studentList;
+  }
+
+  return teachersList;
 
 }
+
+
+
+
+
+
+
+//arrayTeacher = [ {name : , role:,}, {name: john , role: teacher}]
 
 // Do not remove lines below, it is for tests
 module.exports = filteredRoles;
