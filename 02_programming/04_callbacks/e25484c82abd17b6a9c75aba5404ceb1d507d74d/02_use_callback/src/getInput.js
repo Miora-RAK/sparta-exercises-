f95dc2ca20c747eaa8301f7fbc1091a50ab6ea10
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-14 11:48:36 +0100

    add and done exercise use callback

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -1,5 +1,8 @@
 // A function getInput that takes two parameters, an input (the first one) and a callback function
-
-const getInput = // code the function here
-  // Leave the line below for tests to work
-  (module.exports = getInput);
+const getInput = ({user, key},log) => {
+  const result = log("A");
+  console.log (result);
+};
+// code the function here
+// Leave the line below for tests to work
+module.exports = getInput;
