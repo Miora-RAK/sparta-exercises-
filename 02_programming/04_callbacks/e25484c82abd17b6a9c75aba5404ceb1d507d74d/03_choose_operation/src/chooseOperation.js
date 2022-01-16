6d51752a815013e8c68afb59e1158ebd4a09a85d
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-16 22:47:33 +0100

    add and done exercise choose operation

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,22 +1,50 @@
-function addition() {
+function addition(integerOne, integerTwo) {
   // Code here
+  return integerOne + integerTwo;
 }
 
-function substraction() {
+function substraction(integerOne,integerTwo) {
   // Code here
+  return integerOne - integerTwo ;
 }
 
-function multiplication() {
+
+function multiplication(integerOne, integerTwo) {
   // Code here
+  return  integerOne * integerTwo;
 }
 
-function division() {
+function division(integerOne, integerTwo) {
   // Code here
+  return integerOne / integerTwo;
 }
 
-function chooseOperation() {
-  // Code here
+
+function chooseOperation (integerOne, integerTwo, callback ) {
+
+  if(!Number.isInteger(integerOne) || !Number.isInteger(integerTwo) ) {
+    throw new Error("It's not a number");
+  }else {
+    if (callback === "addition") {
+      return addition;
+
+    }else if(callback === "substraction"){
+      return substraction;
+
+    }else if(callback === "multiplication"){
+      return multiplication;
+
+    } else if(callback === "division"){
+      return division;
+    }else {
+      return callback(integerOne,integerTwo) ;
+    }
+  }
+
+
 }
+
+console.log(chooseOperation(9,12,addition));
 
 // Leave the line below for tests to work
 module.exports = { chooseOperation, addition, substraction, multiplication, division };
