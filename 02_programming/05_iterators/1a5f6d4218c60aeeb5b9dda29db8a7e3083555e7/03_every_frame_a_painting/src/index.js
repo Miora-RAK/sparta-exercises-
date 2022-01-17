1ea414e56fffacce7bb2525c291f9ad28abde57f
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-17 15:10:12 +0100

    add and done exercise every frame a painting

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/03_every_frame_a_painting/src/index.js
@@ -1,6 +1,14 @@
 function canTakeArtPieces(pieces) {
-
+  return pieces.every((onePiece) => onePiece.type === "painting");
 }
+
+canTakeArtPieces(
+  [
+    {piece: "Mona Lisa", type: "painting"},
+    {piece: "Guernica", type: "painting"}
+  ]
+);
+
 
 // Do not remove the following line, it is for tests
 module.exports = canTakeArtPieces;
