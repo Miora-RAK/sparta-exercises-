1060f498581f4c5e838ff2b1ed82ea12b19e8a7e
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-17 10:04:39 +0100

    start exercise destructure and spread intermediate

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/05_destructure_and_spread_intermediate/src/index.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/05_destructure_and_spread_intermediate/src/index.js
@@ -9,13 +9,17 @@ let storageHell = [
   ["Coffee Machine", "Azelad"],
 ];
 // Should be cleaned this way => ['IPad', 'IPhone', 'GameBoy color', 'Nes', 'Donkey kong 64', 'Apex Legends Starter Pack', 'LG 5K 27p screen', 'Coffee Machine', 'Azelad']
+const [empty, IpadIphone, GameBoyColor, NesDonkeykong64,ApexLegendsStarterPackLG5K27pscreen,CoffeeMachineAzelad] = storageHell;
 
-function formatStorage() {
-  // Code your function here
+function formatStorage(storageHell) {
+  const formattedArray = [...empty,...IpadIphone,...GameBoyColor,...NesDonkeykong64,...ApexLegendsStarterPackLG5K27pscreen,...CoffeeMachineAzelad];
+  console.log(formattedArray);
+  return formattedArray;
 }
+
 
 // ↓ Feel free to uncomment this next line, or even create more to try everything you do ↓
 // console.log(formatStorage(storageHell));
-
+console.log(formatStorage(storageHell));
 // Don't modify this, it is for the tests.
 module.exports = formatStorage;
