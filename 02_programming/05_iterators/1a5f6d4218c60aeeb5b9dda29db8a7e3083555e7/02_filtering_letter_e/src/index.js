38d43644e0d01298006cfaf20d82f7a0c96bcf46
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-17 11:58:04 +0100

    add and done filtering letter e

--- 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
+++ 02_programming/05_iterators/1a5f6d4218c60aeeb5b9dda29db8a7e3083555e7/02_filtering_letter_e/src/index.js
@@ -1,5 +1,5 @@
 function theEFilter(sentences) {
-
+  return sentences.filter((sentence) => sentence.includes("e")|| sentence.includes("E") ) ;
 }
 
 // Do not remove the following line, it is for tests
