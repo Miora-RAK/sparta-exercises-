9815559032edba793b1a5eaeea6254cca18e8920
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-18 10:46:26 +0100

    add and done exercise for each with a loop

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/02_for_each_with_a_loop/src/index.js
@@ -1,7 +1,14 @@
-function forEachLoop() {
-  // Code the function here.
+const numbers = [1, 2, 3, 4];
+function forEachLoop(numbers,log) {
+
+  for(let i = 0; i < numbers.length; i++){
+    log( numbers[i]);
+  }
+
+
 
 }
+
 
 // Do not remove the following lines, it is for tests
 module.exports = forEachLoop;
