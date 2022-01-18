025b6217390d02b56e1ae58180625489295ee050
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-18 11:44:37 +0100

    add and done exercise map with a loop

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/03_map_with_a_loop/src/index.js
@@ -1,7 +1,13 @@
-function mapLoop() {
-  // Code the function here.
+const numbers = [1, 2, 3, 4];
 
+function mapLoop(numbers, double) {
+  let doubleNumbers = [];
+  for (let i = 0; i < numbers.length; i++){
+    doubleNumbers.push(double(numbers[i]));
+  }
+  return doubleNumbers;
 }
+
 
 // Do not remove the following lines, it is for tests
 module.exports = mapLoop;
