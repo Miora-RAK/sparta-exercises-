18b2603b1ab3525a71b5119df29613ea99390a02
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-19 18:31:03 +0100

    start exercise number game with stats

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/04_number_game_with_stats/src/numberGameWithStats.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/04_number_game_with_stats/src/numberGameWithStats.js
@@ -1,5 +1,36 @@
 function numberGameWithStats(reader, min = 1, max = 100) {
-  // Code here
+  console.log("Welcome ! \nYou have to find the right number, \nbetween 1 and 100! \nGood luck!!");
+  let randomNumber = Math.round(Math.random() * (max - min) + min);
+  console.log(randomNumber);
+  let numberTry = 0;
+
+  const gameQuestion = () => {
+    return reader.question("Enter a number", (number) => {
+      if (isNaN(number)) {
+        console.log(`${number} \nThis was not a number`);
+        gameQuestion();
+        numberTry += 1;
+      } else {
+        if (number < min || number > max) {
+          console.log(`${number} \nThe number is between 1 and 100`);
+          gameQuestion();
+          numberTry += 1;
+        } else if (number < randomNumber) {
+          console.log(`${number} \nToo low`);
+          gameQuestion();
+          numberTry += 1;
+        } else if (number > randomNumber) {
+          console.log(`${number} \nToo high`);
+          gameQuestion();
+          numberTry += 1;
+        } else if (number === randomNumber) {
+          console.log(`${number} \nYou won! ${numberTry}`);
+          reader.close();
+        }
+      }
+    });
+  };
+  gameQuestion();
 }
 
 module.exports = numberGameWithStats;
