3c36512680ba9e53248a76a604ad8d05dce5fcc3
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-19 10:04:23 +0100

    start exercise draw me a square

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/05_draw_me_a_square/src/index.js
@@ -1,6 +1,12 @@
-function square() {
-  // Code here
+function square(squareSize) {
+  let star = "";
+  for (let i = squareSize; i <= squareSize; i++) {
+    for (let j = i; j <= squareSize; j++) {
+      console.log((star += "*".repeat(i)));
+    }
+    console.log("*".repeat(i));
+  }
 }
-
+square(5);
 // Do not remove the following line, it is for tests
 module.exports = square;
