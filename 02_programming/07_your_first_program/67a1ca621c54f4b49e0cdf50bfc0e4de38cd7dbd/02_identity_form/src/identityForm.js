f13dccb9cd526498950aae4af91c0148446a4cbd
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-19 10:34:50 +0100

    add and done exercise identity forms

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/02_identity_form/src/identityForm.js
@@ -1,5 +1,12 @@
 const identityForm = (reader) => {
-  // Code here
+  reader.question("first name ", (firstName) => {
+    reader.question("last name ", (lastName) => {
+      reader.question("age ", (age) => {
+        console.log(`Your name is ${firstName} ${lastName} and you are ${age}.`);
+        reader.close();
+      });
+    });
+  });
 };
 
 // Leave line below for tests to work
