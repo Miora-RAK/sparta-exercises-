bb5882f0bc9c625b148fd4d37660c1c09f05cd05
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-19 17:02:43 +0100

    add and done exercise number game

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
@@ -1,5 +1,33 @@
 function numberGame(reader, min = 1, max = 100) {
-  // code here
+  console.log("Welcome ! \nYou have to find the right number, \nbetween 1 and 100! \nGood luck!!");
+  let randomNumber = Math.round(Math.random() * (max - min) + min);
+
+  const gameQuestion = () => {
+    return reader.question("Enter a number", (number) => {
+      if (isNaN(number)) {
+        console.log(`${number} \nThis was not a number`);
+        gameQuestion();
+      } else {
+        if (number < min || number > max) {
+          console.log(`${number} \nThe number is between 1 and 100`);
+          gameQuestion();
+        } else if (number < randomNumber) {
+          console.log(`${number} \nToo low`);
+          gameQuestion();
+        } else if (number > randomNumber) {
+          console.log(`${number} \nToo high`);
+          gameQuestion();
+        } else if (number === randomNumber) {
+          console.log(`${number} \nYou won!`);
+          reader.close();
+        }
+      }
+    });
+  };
+  gameQuestion();
+}
+function loopQuestion(reader) {
+  reader.question("Bien essayé , try again! ", (secondTry) => {});
 }
 
 module.exports = numberGame;
