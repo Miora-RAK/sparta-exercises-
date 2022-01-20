14673a180ebb8871e01b67665afdd2904c3842c6
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-20 15:59:35 +0100

    add and done  exercise chaining

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/03_chaining/src/car.js
@@ -1,9 +1,34 @@
 // Complete and add needed car properties:
 const car = {
   speed: 0,
+  minutes: 0,
+  distance: 0,
+
   start: function () {
-  
+    this.speed = 0;
+    this.minutes = 0;
+    this.distance = 0;
+    return this;
+  },
+  changeSpeed: function (speed) {
+    this.speed = speed;
+    return this;
+  },
+  drive: function (minutes) {
+    if (this.distance === 0) {
+      this.minutes = minutes;
+      this.distance = (this.speed * this.minutes) / 60;
+    } else {
+      this.minutes = minutes;
+      this.distance = this.distance + (this.speed * this.minutes) / 60;
+    }
+    return this;
+  },
+  showDistance: function () {
+    console.log(`${this.distance} Km`);
+    return this;
   },
 };
+car.start().changeSpeed(130).drive(42).showDistance();
 
 module.exports = car;
