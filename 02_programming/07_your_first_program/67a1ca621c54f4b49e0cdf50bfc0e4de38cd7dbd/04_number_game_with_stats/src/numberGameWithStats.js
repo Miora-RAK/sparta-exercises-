97a487cb7ac0a7906eaaf86df2637025e8066490
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-20 05:45:00 +0100

    add number of tries to exercise number game with stats

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/04_number_game_with_stats/src/numberGameWithStats.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/04_number_game_with_stats/src/numberGameWithStats.js
@@ -3,28 +3,34 @@ function numberGameWithStats(reader, min = 1, max = 100) {
   let randomNumber = Math.round(Math.random() * (max - min) + min);
   console.log(randomNumber);
   let numberTry = 0;
+  let numberTryList = [];
 
   const gameQuestion = () => {
     return reader.question("Enter a number", (number) => {
       if (isNaN(number)) {
-        console.log(`${number} \nThis was not a number`);
+        console.log(`${number} \nThis was not a number\nNumber of tries : ${numberTryList}`);
         gameQuestion();
+        numberTryList.push(number);
         numberTry += 1;
       } else {
         if (number < min || number > max) {
-          console.log(`${number} \nThe number is between 1 and 100`);
+          console.log(`${number} \nThe number is between 1 and 100 \nNumber of tries : ${numberTryList}`);
           gameQuestion();
+          numberTryList.push(number);
           numberTry += 1;
         } else if (number < randomNumber) {
-          console.log(`${number} \nToo low`);
+          console.log(`${number} \nToo low \nNumber of tries : ${numberTryList}`);
           gameQuestion();
+          numberTryList.push(number);
           numberTry += 1;
         } else if (number > randomNumber) {
-          console.log(`${number} \nToo high`);
+          console.log(`${number} \nToo high \nNumber of tries : ${numberTryList}`);
           gameQuestion();
+          numberTryList.push(number);
           numberTry += 1;
         } else if (number === randomNumber) {
           console.log(`${number} \nYou won! ${numberTry}`);
+          console.log(`You guessed it in ${numberTry} tries`);
           reader.close();
         }
       }
