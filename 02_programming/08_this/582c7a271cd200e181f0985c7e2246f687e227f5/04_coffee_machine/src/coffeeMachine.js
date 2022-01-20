c4e540f2a4924485eec8b918ef5d6355dee05dc2
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-20 16:49:14 +0100

    add and done coffe machine

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/04_coffee_machine/src/coffeeMachine.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/04_coffee_machine/src/coffeeMachine.js
@@ -1,5 +1,26 @@
 const machine = {
-  // Complete here
-};
+  litersOfCoffee: 10,
 
+  fillWithLitersOfCoffee: function (litersOfCoffee) {
+    this.litersOfCoffee += litersOfCoffee;
+    return this;
+  },
+  expresso: function () {
+    if (this.litersOfCoffee !== 0) {
+      this.litersOfCoffee = this.litersOfCoffee - 0.08;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  longCoffee: function () {
+    if (this.litersOfCoffee !== 0) {
+      this.litersOfCoffee = this.litersOfCoffee - 0.15;
+      return true;
+    } else {
+      return false;
+    }
+  },
+};
+machine.fillWithLitersOfCoffee(10);
 module.exports = machine;
