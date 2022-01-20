c91b41649ab9baf07a65188e05049d508521800b
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-20 11:13:57 +0100

    add and done exercise fundamental flowchart

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/01_fundamental_flowchart/src/index.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/01_fundamental_flowchart/src/index.js
@@ -1,11 +1,11 @@
 const shouldIMakeATicket = require("./shouldIMakeATicket");
 
-const answerOne = shouldIMakeATicket(true, 35, 10)
+const answerOne = shouldIMakeATicket(true, 35, 10);
 
 console.log(answerOne);
 // Should console log "true"
 
-const answerTwo = shouldIMakeATicket(true, 5, 15)
+const answerTwo = shouldIMakeATicket(true, 5, 15);
 
-console.log(answerTwo)
+console.log(answerTwo);
 // Should console log "false"
--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/01_fundamental_flowchart/src/shouldIMakeATicket.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/01_fundamental_flowchart/src/shouldIMakeATicket.js
@@ -1,8 +1,15 @@
-function shouldIMakeATicket(isStuck, timeBeingStuck, hourOfTheDay){
-
-  // Code your function here
-
+function shouldIMakeATicket(isStuck, timeBeingStuck, hourOfTheDay) {
+  if (
+    isStuck === true &&
+    timeBeingStuck >= 20 &&
+    ((9 <= hourOfTheDay && hourOfTheDay < 13) || (14 <= hourOfTheDay && hourOfTheDay < 18))
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
+shouldIMakeATicket();
 
 // Don't touch this for tests to execute properly
 module.exports = shouldIMakeATicket;
