93b4088bb35f589fac64fe6c3107608f2cc932da
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-23 23:45:36 +0100

    add and done exercise destructure and spread intermediate

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/05_destructure_and_spread_intermediate/src/index.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/05_destructure_and_spread_intermediate/src/index.js
@@ -9,14 +9,27 @@ let storageHell = [
   ["Coffee Machine", "Azelad"],
 ];
 // Should be cleaned this way => ['IPad', 'IPhone', 'GameBoy color', 'Nes', 'Donkey kong 64', 'Apex Legends Starter Pack', 'LG 5K 27p screen', 'Coffee Machine', 'Azelad']
-const [empty, IpadIphone, GameBoyColor, NesDonkeykong64,ApexLegendsStarterPackLG5K27pscreen,CoffeeMachineAzelad] = storageHell;
+const [
+  empty,
+  IpadIphone,
+  GameBoyColor,
+  NesDonkeykong64,
+  ApexLegendsStarterPackLG5K27pscreen,
+  CoffeeMachineAzelad,
+] = storageHell;
 
 function formatStorage(storageHell) {
-  const formattedArray = [...empty,...IpadIphone,...GameBoyColor,...NesDonkeykong64,...ApexLegendsStarterPackLG5K27pscreen,...CoffeeMachineAzelad];
-  console.log(formattedArray);
+  const formattedArray = [
+    ...storageHell[0],
+    ...storageHell[1],
+    ...storageHell[2],
+    ...storageHell[3],
+    ...storageHell[4],
+    ...storageHell[5],
+  ];
+
   return formattedArray;
 }
-
 
 // ↓ Feel free to uncomment this next line, or even create more to try everything you do ↓
 // console.log(formatStorage(storageHell));
