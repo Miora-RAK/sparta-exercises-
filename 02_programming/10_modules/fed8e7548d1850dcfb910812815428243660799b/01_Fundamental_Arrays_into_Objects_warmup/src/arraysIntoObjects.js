572002471e95a872c2cfa6dd39803436ad84eeb2
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-24 14:02:35 +0100

    modification exercise fundamental arrays into objects warmup

--- 02_programming/10_modules/fed8e7548d1850dcfb910812815428243660799b/01_Fundamental_Arrays_into_Objects_warmup/src/arraysIntoObjects.js
+++ 02_programming/10_modules/fed8e7548d1850dcfb910812815428243660799b/01_Fundamental_Arrays_into_Objects_warmup/src/arraysIntoObjects.js
@@ -1,11 +1,17 @@
 const arraysIntoObjects = (keysArray, valuesArray) => {
-  // let allArray = { ...keysArray, ...valuesArray };
-  return {
-    name: valuesArray[0],
-    job: valuesArray[1],
-    age: valuesArray[2],
-  };
+  let obj = {};
+  keysArray.forEach((key, value) => (obj[key] = valuesArray[value]));
+  return obj;
 };
+
+//  Object.keys(obj).map(function(keysArray,valuesArray){
+// }
+
+// return {
+//   name: valuesArray[0],
+//   job: valuesArray[1],
+//   age: valuesArray[2],
+// };
 
 // Don't change this line for tests to run properly
 export default arraysIntoObjects;
