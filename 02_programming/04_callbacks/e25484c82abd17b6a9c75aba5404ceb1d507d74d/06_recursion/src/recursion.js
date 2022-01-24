35d7026ec7622cae8249dff063a8226aa5e20c29
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-24 08:26:52 +0100

    start exercise recursion

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
@@ -1,5 +1,12 @@
-const findPosition = () => {
-  // Code here
+const personList = ["Fenn", "Florian", "Martin", "Louis", "Thomas"];
+
+const findPosition = (personList, name, position) => {
+  if (personList.indexOf(personList["Fenn"]) === -1) {
+    console.log(`${name} is not present in this array.`);
+  } else {
+    console.log(`${name} is at position #${position} in this array.`);
+  }
+  return findPosition();
 };
 
 // Leave line below for tests to work properly
