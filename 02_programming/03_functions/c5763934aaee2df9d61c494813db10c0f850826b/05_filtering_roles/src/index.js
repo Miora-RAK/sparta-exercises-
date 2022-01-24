4a41c8b5795defcbef6f220962c7f385827619da
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-24 08:25:48 +0100

    start exercise filtering roles

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,25 +1,73 @@
-function filteredRoles(personList,role = "Teacher"){
-
+const personList = [
+  {
+    name: "Frieda",
+    role: "Teacher",
+  },
+  {
+    name: "John",
+    role: "Teacher",
+  },
+  {
+    name: "Francis",
+    role: "Student",
+  },
+  {
+    name: "Jane",
+    role: "Student",
+  },
+];
+const studentsList = personList.filter((person) => person.role === "Student");
+const teachersList = personList.filter((person) => person.role === "Teacher");
+function filteredRoles(personList, role = "Teacher") {
   console.log(personList);
   console.log(role);
-  const studentList = personList.filter(personList => personList.role === "Student");
-  const teachersList = personList.filter(personList => personList.role === "Teacher");
-  const emptyArray = [{}];
-  if (role === "Teacher"){
-    console.log(emptyArray) ;
-  }else {
-    return studentList;
+  let emptyArray = [];
+  if (personList === "Student") {
+    if (personList[0].role === "Student") {
+      console.log(emptyArray.push(personList[0]));
+    } else if (personList[1].role === "Student") {
+      console.log(emptyArray.push(personList[1]));
+    } else if (personList[2].role === "Student") {
+      console.log(emptyArray.push(personList[2]));
+    } else if (personList[3].role === "Student") {
+      console.log(emptyArray.push(personList[3]));
+    } else {
+      console.log(emptyArray);
+    }
+  } else {
+    if (personList[0].role === "Teacher") {
+      console.log(emptyArray.push(personList[0]));
+    } else if (personList[1].role === "Teacher") {
+      console.log(emptyArray.push(personList[1]));
+    } else if (personList[2].role === "Teacher") {
+      console.log(emptyArray.push(personList[2]));
+    } else if (personList[3].role === "Teacher") {
+      console.log(emptyArray.push(personList[3]));
+    } else {
+      console.log(emptyArray);
+    }
   }
 
-  return teachersList;
+  // if (personList[0].role === "Student") {
+  //   emptyArray.push(personList[0]);
+  // } else if (personList[1].role === "Student") {
+  //   emptyArray.push(personList[1]);
+  // } else if (personList[2].role === "Student") {
+  //   emptyArray.push(personList[2]);
+  // } else if (personList[3].role === "Student") {
+  //   emptyArray.push(personList[3]);
+  // } else {
+  //   emptyArray;
+  // }
 
+  // for (let i = 0; i < personList.length; i++) {
+  //   let person = personList[i];
+  //   if (personList.role === "Teacher") {
+  //     emptyArray.push(person);
+  //   }
+  // }
+  return emptyArray;
 }
-
-
-
-
-
-
 
 //arrayTeacher = [ {name : , role:,}, {name: john , role: teacher}]
 
