ead9916f3048da9c64178f3503cb8c2599b31e91
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-25 13:57:41 +0100

    add and done exercise checking a spacecraft

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,5 +1,22 @@
-// Code here
+const stealthyFuelLevelCheck = (integer) => {
+  if (integer > 20) {
+    return "Fuel level: green";
+  } else if (integer > 10) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
+};
 
+const stealthyCargoHoldCheck = (cargo) => {
+  if (cargo.length === 4) {
+    return "Full";
+  } else if (cargo.length < 4) {
+    return `Spaces available: ${4 - cargo.length}`;
+  } else if (cargo.length > 4) {
+    return `Over capacity by ${cargo.length - 4} items.`;
+  }
+  return "string";
+};
 // Do not remove last lines, it is for tests
-
 module.exports = { stealthyFuelLevelCheck, stealthyCargoHoldCheck };
