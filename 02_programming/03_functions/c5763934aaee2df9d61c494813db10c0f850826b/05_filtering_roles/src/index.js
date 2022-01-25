90f7a7715c8ece84ba5e6f7556ae0f1f068778d4
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-25 12:50:33 +0100

    add and done filtering roles

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -16,57 +16,12 @@ const personList = [
     role: "Student",
   },
 ];
-const studentsList = personList.filter((person) => person.role === "Student");
-const teachersList = personList.filter((person) => person.role === "Teacher");
+
 function filteredRoles(personList, role = "Teacher") {
-  console.log(personList);
-  console.log(role);
   let emptyArray = [];
-  if (personList === "Student") {
-    if (personList[0].role === "Student") {
-      console.log(emptyArray.push(personList[0]));
-    } else if (personList[1].role === "Student") {
-      console.log(emptyArray.push(personList[1]));
-    } else if (personList[2].role === "Student") {
-      console.log(emptyArray.push(personList[2]));
-    } else if (personList[3].role === "Student") {
-      console.log(emptyArray.push(personList[3]));
-    } else {
-      console.log(emptyArray);
-    }
-  } else {
-    if (personList[0].role === "Teacher") {
-      console.log(emptyArray.push(personList[0]));
-    } else if (personList[1].role === "Teacher") {
-      console.log(emptyArray.push(personList[1]));
-    } else if (personList[2].role === "Teacher") {
-      console.log(emptyArray.push(personList[2]));
-    } else if (personList[3].role === "Teacher") {
-      console.log(emptyArray.push(personList[3]));
-    } else {
-      console.log(emptyArray);
-    }
-  }
-
-  // if (personList[0].role === "Student") {
-  //   emptyArray.push(personList[0]);
-  // } else if (personList[1].role === "Student") {
-  //   emptyArray.push(personList[1]);
-  // } else if (personList[2].role === "Student") {
-  //   emptyArray.push(personList[2]);
-  // } else if (personList[3].role === "Student") {
-  //   emptyArray.push(personList[3]);
-  // } else {
-  //   emptyArray;
-  // }
-
-  // for (let i = 0; i < personList.length; i++) {
-  //   let person = personList[i];
-  //   if (personList.role === "Teacher") {
-  //     emptyArray.push(person);
-  //   }
-  // }
-  return emptyArray;
+  const teachersList = personList.filter((person) => person.role === role);
+  console.log(teachersList);
+  return teachersList;
 }
 
 //arrayTeacher = [ {name : , role:,}, {name: john , role: teacher}]
