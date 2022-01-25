2ccc8dc4b158abfbd470af640a285e8e9a62c41b
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-25 13:51:30 +0100

    add and done exercise preparing a spacecraft

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
@@ -1,9 +1,22 @@
-function checkFuelLevel() {
-  // Code here
+function checkFuelLevel(integer) {
+  if (integer > 20) {
+    return "Fuel level: green";
+  } else if (integer > 10) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
 }
 
-function checkCargoHold(){
-  // Code here
+function checkCargoHold(cargo) {
+  if (cargo.length === 4) {
+    return "Full";
+  } else if (cargo.length < 4) {
+    return `Spaces available: ${4 - cargo.length}`;
+  } else if (cargo.length > 4) {
+    return `Over capacity by ${cargo.length - 4} items.`;
+  }
+  return "string";
 }
 
 // Do not remove last lines, it is for tests
