34fa7efee562b6834d2392664fdd6634a0490835
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-26 14:52:41 +0100

    add and done draw me a triangle

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/06_draw_me_a_triangle/src/index.js
@@ -1,6 +1,10 @@
-function triangle() {
-  // Code here
+function triangle(squareSize) {
+  let star = "";
+  for (let i = 1; i <= squareSize; i++) {
+    console.log((star += "*"));
+  }
 }
+console.log(triangle(2));
 
 // Do not remove the following line, it is for tests
 module.exports = triangle;
