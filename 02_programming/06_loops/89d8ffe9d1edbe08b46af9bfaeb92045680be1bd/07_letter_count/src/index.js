1582876a639de5085023d77f58b93f6f27e4718c
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-26 15:12:54 +0100

    add and done exercise letter count

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/07_letter_count/src/index.js
@@ -1,6 +1,13 @@
-function letterCount() {
-  // Code here
+function letterCount(word, letter) {
+  let counter = 0;
+  for (let i = 0; i <= word.length; i++) {
+    if (word[i] === letter) {
+      counter++;
+    }
+  }
+  return counter;
 }
+console.log(letterCount("gattaca", "a"));
 
 // Do not remove the following line, it is for tests
 module.exports = letterCount;
