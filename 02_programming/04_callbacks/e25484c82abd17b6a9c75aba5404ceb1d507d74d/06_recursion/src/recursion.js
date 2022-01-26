e3406dd45ab2465e1e9a695281835997deae4673
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-26 14:16:40 +0100

    add and done exercise recursion

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
@@ -1,13 +1,26 @@
 const personList = ["Fenn", "Florian", "Martin", "Louis", "Thomas"];
 
-const findPosition = (personList, name, position) => {
-  if (personList.indexOf(personList["Fenn"]) === -1) {
-    console.log(`${name} is not present in this array.`);
+function arrayString(array) {
+  return array.every((element) => typeof element === "string");
+}
+
+const findPosition = (personList, name, index) => {
+  if (Number.isInteger(index) && typeof name === "string" && arrayString(personList)) {
+    if (index < personList.length) {
+      if (name === personList[index]) {
+        return `${name} is at position #${index + 1} in this array.`;
+      } else {
+        return findPosition(personList, name, index + 1);
+      }
+    } else {
+      return `${name} is not present in this array.`;
+    }
   } else {
-    console.log(`${name} is at position #${position} in this array.`);
+    throw new Error(`${name} is not present in this array.`);
   }
-  return findPosition();
 };
 
-// Leave line below for tests to work properly
+const myArray = ["Alpha", "Beta", "Delta", "Gamma"];
+console.log(findPosition(myArray, "Delta", 0));
+
 module.exports = findPosition;
