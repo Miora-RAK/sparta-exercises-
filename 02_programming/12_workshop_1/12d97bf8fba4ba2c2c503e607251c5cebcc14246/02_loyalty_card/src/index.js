ee0ec87c7b735c39599403c70ad3b8aee2433ccf
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-31 10:17:46 +0100

    start loyalty card

---
+++ 02_programming/12_workshop_1/12d97bf8fba4ba2c2c503e607251c5cebcc14246/02_loyalty_card/src/index.js
@@ -0,0 +1,9 @@
+import readline from "readline";
+import * as uuid from "uuid";
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const id = uuid.v4(); // af7bce0f-0f44-4b54-b333-7ea46ddb5b3d
