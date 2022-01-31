4ef0b3772d1a724e7c951de188296da568cd6aa2
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-31 11:36:35 +0100

    add and done typed function with type alias

--- 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
+++ 02_programming/15_typescript_101/f284fbcc79bce170e7c536c6c6a19b31653e10bb/03_typed_function_with_type_alias/src/index.ts
@@ -1,6 +1,11 @@
-function handleTuple() {
-  // Code the function here.
+function handleTuple(tuple: MyTuple): void {
+  if (typeof tuple[0] === "string") {
+    console.log(`${tuple[0]} is a string`);
+    console.log(`${tuple[1]} is a number`);
+  }
 }
+
+type MyTuple = [string, number];
 
 // Leave the line below for tests to work properly.
 export { handleTuple };
