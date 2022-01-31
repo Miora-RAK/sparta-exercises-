8e328093521b6f7e978a57158e14130f6a623e1b
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-01-31 10:17:18 +0100

    start reboot calculator

--- 02_programming/12_workshop_1/12d97bf8fba4ba2c2c503e607251c5cebcc14246/01_reboot_calculator/src/calculator.js
+++ 02_programming/12_workshop_1/12d97bf8fba4ba2c2c503e607251c5cebcc14246/01_reboot_calculator/src/calculator.js
@@ -5,40 +5,62 @@ import { subtraction } from "./operation/subtraction.js";
 import { multiplication } from "./operation/multiplication.js";
 import { division } from "./operation/division.js";
 
+const chooseOperation = (numberOne, numberTwo, operation) => {
+  if (!Number.isInteger(numberOne) || !Number.isInteger(numberTwo) || operation !== "string") {
+    throw new Error("It's not the rigth type! Try again!! ;) ");
+  } else {
+    if (operation === "+") {
+      console.log(`${numberOne} ${operation} ${numberTwo} = ${addition(Number(numberOne), Number(numberTwo))}`);
+    } else if (operation === "-") {
+      console.log(`${numberOne} ${operation} ${numberTwo} = ${subtraction(Number(numberOne), Number(numberTwo))}`);
+    } else if (operation === "*") {
+      console.log(`${numberOne} ${operation} ${numberTwo} = ${multiplication(Number(numberOne), Number(numberTwo))}`);
+    } else if (operation === "/") {
+      console.log(`${numberOne} ${operation} ${numberTwo} = ${division(Number(numberOne), Number(numberTwo))}`);
+    } else {
+      operation(numberOne, numberTwo);
+      reader.closer();
+    }
+  }
+};
+
 start();
+let signs = ["+", "-", "*", "/"];
 function calculator() {
   reader.question("Enter the first number\n> ", (firstNumber) => {
     reader.question("Choose an operation: [ + - * / ] \n> ", (operation) => {
       reader.question("Enter the second number\n> ", (secondNumber) => {
-        // if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
-        //   throw new Error("It's not a number");
+        const result = chooseOperation(firstNumber, secondNumber, operation);
+        // if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber) || operation !== "string") {
+        //   throw new Error("It's not the rigth type! Try again!! ;) ");
+
         // } else {
-        if (operation === "+") {
-          console.log(
-            `${firstNumber} ${operation} ${secondNumber} = ${addition(Number(firstNumber), Number(secondNumber))}`,
-          );
-        }
-        if (operation === "-") {
-          console.log(
-            `${firstNumber} ${operation} ${secondNumber} = ${subtraction(Number(firstNumber), Number(secondNumber))}`,
-          );
-        }
-        if (operation === "*") {
-          console.log(
-            `${firstNumber} ${operation} ${secondNumber} = ${multiplication(
-              Number(firstNumber),
-              Number(secondNumber),
-            )}`,
-          );
-        }
-        if (operation === "/") {
-          console.log(
-            `${firstNumber} ${operation} ${secondNumber} = ${division(Number(firstNumber), Number(secondNumber))}`,
-          );
-          // } else {
-          //   return operation(firstNumber, secondNumber);
-          // }
-        }
+        //   if (operation === "+") {
+        //     console.log(
+        //       `${firstNumber} ${operation} ${secondNumber} = ${addition(Number(firstNumber), Number(secondNumber))}`,
+        //     );
+        //   }
+        //   if (operation === "-") {
+        //     console.log(
+        //       `${firstNumber} ${operation} ${secondNumber} = ${subtraction(Number(firstNumber), Number(secondNumber))}`,
+        //     );
+        //   }
+        //   if (operation === "*") {
+        //     console.log(
+        //       `${firstNumber} ${operation} ${secondNumber} = ${multiplication(
+        //         Number(firstNumber),
+        //         Number(secondNumber),
+        //       )}`,
+        //     );
+        //   }
+        //   if (operation === "/") {
+        //     console.log(
+        //       `${firstNumber} ${operation} ${secondNumber} = ${division(Number(firstNumber), Number(secondNumber))}`,
+        //     );
+        //   }
+        // } else {
+        //   return operation(firstNumber, secondNumber);
+        // }
       });
     });
   });
