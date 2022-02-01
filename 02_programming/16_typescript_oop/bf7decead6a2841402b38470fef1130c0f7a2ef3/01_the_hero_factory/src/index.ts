df446fb4687d79c6fe7df47345168788a16c121c
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-01 09:57:41 +0100

    add and done exercise the hero factory

--- 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/01_the_hero_factory/src/index.ts
+++ 02_programming/16_typescript_oop/bf7decead6a2841402b38470fef1130c0f7a2ef3/01_the_hero_factory/src/index.ts
@@ -1,4 +1,14 @@
 // Code the class here.
+class Hero {
+  name: string;
 
+  constructor(name: string) {
+    this.name = name;
+  }
+  sayHi(): string {
+    return `I'm ${this.name}`;
+  }
+}
+const batman = new Hero("Batman");
 // Leave the line below for tests to work properly.
 export { Hero };
