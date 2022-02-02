ed083232b2aa000a7288edaa74340039ef871fe2
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-02 14:12:08 +0100

    add and done exercise become a dev

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/03_become_a_dev/src/index.ts
@@ -1,4 +1,20 @@
+type Human = {
+  name: string;
+  age: number;
+  knowsDev: boolean;
+};
+type Dev = Human & {
+  knownLanguage: string;
+};
 
+function becomeADev(object: Human): Dev {
+  const devObject: Dev = {
+    ...object,
+    knowsDev: true,
+    knownLanguage: "JavaScript",
+  };
+  return devObject;
+}
 
 // Leave the line below for tests to work properly.
 export { becomeADev };
