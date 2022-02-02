e7a2576c55eb256094c3864b862b3ebd1cc424e0
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-02 12:47:58 +0100

    add and done exercise object transformer

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/02_object_transformer/src/index.ts
@@ -1,3 +1,22 @@
+type Product = {
+  productName: string;
+  quantity: number;
+};
+
+function productTransformer(product: Product): [string, string | number][] {
+  return Object.entries(product);
+}
+
+// ou
+// function productTransformer(product: Product): [string, string | number][] {
+//  const productTransformArray = [];
+// const productNameArray = [];
+// const quantityArray = [];
+// productNameArray.push(Object.keys(product)[0], product.productName);
+// quantityArray.push(Object.keys(product)[1], product.quantity);
+// productTransformArray.push(productNameArray, quantityArray);
+// return productTransformArray;
+// }
 
 // Leave the line below for tests to work properly.
 export { productTransformer };
