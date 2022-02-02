4f04e99f002cfff99d655efc2aa476f8011d7cd8
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-02 10:26:40 +0100

    add and done exercise fixing a bad data

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/01_fixing_a_bad_data/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/01_fixing_a_bad_data/src/index.ts
@@ -1,7 +1,18 @@
-// const receivedObject = {
-//   a: 'somestring',
-//   b: 42
-// }
+type ReceivedObject = {
+  a: string;
+  b: number;
+};
+
+const receivedObject: ReceivedObject = {
+  a: "somestring",
+  b: 42,
+};
+
+function transformObject(receivedObject: ReceivedObject): (string | number)[] {
+  const newArray = [];
+  newArray.push(receivedObject.a, receivedObject.b);
+  return newArray;
+}
 
 // Leave the line below for tests to work properly.
 export { transformObject };
