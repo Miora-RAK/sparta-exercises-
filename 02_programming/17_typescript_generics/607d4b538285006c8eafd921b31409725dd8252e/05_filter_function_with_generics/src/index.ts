94377381fb08e02627d31dfc87512f6d41243f9c
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-02 15:03:09 +0100

    add and done exercise fileter function with generics

--- 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
+++ 02_programming/17_typescript_generics/607d4b538285006c8eafd921b31409725dd8252e/05_filter_function_with_generics/src/index.ts
@@ -1,13 +1,16 @@
-function filterFor(array, predicate) {
+function filterFor<FilterForType>(
+  array: FilterForType[],
+  predicate: (element: FilterForType) => boolean,
+): FilterForType[] {
   const resultArray = [];
 
   for (const element of array) {
     if (predicate(element)) {
-      resultArray.push(element)
+      resultArray.push(element);
     }
   }
 
   return resultArray;
 }
 
-export { filterFor }
+export { filterFor };
