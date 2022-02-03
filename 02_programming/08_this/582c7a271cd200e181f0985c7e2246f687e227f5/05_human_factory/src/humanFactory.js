0ddd82c0f7bf95a09fed0c31aab8031e55236e2f
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-03 09:34:47 +0100

    start exercise human factory

--- 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/05_human_factory/src/humanFactory.js
+++ 02_programming/08_this/582c7a271cd200e181f0985c7e2246f687e227f5/05_human_factory/src/humanFactory.js
@@ -1,10 +1,37 @@
-const humanFactory = (humanData = {}) => {
+const humanFactory = (humanData = {firstname: "John",
+lastName: "Doe",
+genre: "male",
+job: "unemployed",
+fullname:function () {
+  return`${this.firstname} ${this.lastName}`},
+
+introduction:function() {
+  return `Hello! My name is ${this.fullname}.`},
+
+) => {
+
   // code here
+  if (human.genre === "female"){
+    human.firstName = "Jane";
+    human.genre = "female";
+  }
+  if (human.genre === "")
+
 };
 
 const createHumans = (humans) => {
+  // const [jane, john] = createHumans([{ genre: "female" }, {}]);
   // code here
 };
+
+const human = humanFactory();
+human.firstName; // "John"
+human.lastName; // "Doe"
+human.genre; // "male"
+human.job; // "unemployed"
+human.fullname(); // "John Doe"
+human.introduction(); // "Hello! My name is John Doe."
+
 
 module.exports = {
   humanFactory,
