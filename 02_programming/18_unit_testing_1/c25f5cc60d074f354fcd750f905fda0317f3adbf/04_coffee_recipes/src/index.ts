45fe00e435f067268fe40c8dfed2e1ad2e41d359
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-04 10:55:57 +0100

    add and done exercise coffee recipes

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/04_coffee_recipes/src/index.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/04_coffee_recipes/src/index.ts
@@ -1,3 +1,42 @@
-export function giveRecipe() {
+type CoffeeRecipe = {
+  name: string;
+  ingredients: string[];
+};
+
+const coffeeRecipes: CoffeeRecipe[] = [
+  {
+    name: "espresso",
+    ingredients: ["espresso"],
+  },
+  {
+    name: "macchiato",
+    ingredients: ["espresso", "milk foam"],
+  },
+  {
+    name: "latte",
+    ingredients: ["espresso", "milk foam", "steamed milk"],
+  },
+  {
+    name: "cappuccino",
+    ingredients: ["espresso", "milk foam", "steamed milk", "chocolate"],
+  },
+  {
+    name: "americano",
+    ingredients: ["espresso", "water"],
+  },
+];
+
+export function giveRecipe(name: string): string {
   //code here
+  let recipe;
+  for (let i = 0; i < coffeeRecipes.length; i++) {
+    console.log(i);
+    if (coffeeRecipes[i].name === name) {
+      return (recipe = `${coffeeRecipes[i].name} ingredients:\n- ${coffeeRecipes[i].ingredients.join(",\n- ")}`);
+    }
+  }
+
+  return (recipe = `Sorry, ${name} does not exist on our list.`);
 }
+
+// giveRecipe.length = 1
