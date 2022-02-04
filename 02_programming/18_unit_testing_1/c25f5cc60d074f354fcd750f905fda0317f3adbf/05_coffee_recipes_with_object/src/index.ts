40062f57222fe2ba807811daf3e4d758642652b5
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-04 12:07:46 +0100

    add and done coffee recipes with object

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/05_coffee_recipes_with_object/src/index.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/05_coffee_recipes_with_object/src/index.ts
@@ -1,3 +1,39 @@
 export const recipeMaker = {
-  //code here
+  coffeeRecipes: [
+    {
+      name: "espresso",
+      ingredients: ["espresso"],
+    },
+    {
+      name: "macchiato",
+      ingredients: ["espresso", "milk foam"],
+    },
+    {
+      name: "latte",
+      ingredients: ["espresso", "milk foam", "steamed milk"],
+    },
+    {
+      name: "cappuccino",
+      ingredients: ["espresso", "milk foam", "steamed milk", "chocolate"],
+    },
+    {
+      name: "americano",
+      ingredients: ["espresso", "water"],
+    },
+  ],
+
+  giveRecipe: function giveRecipe(name: string): string {
+    let recipe;
+    for (let i = 0; i < this.coffeeRecipes.length; i++) {
+      if (this.coffeeRecipes[i].name === name) {
+        return (recipe = `${this.coffeeRecipes[i].name} ingredients:\n- ${this.coffeeRecipes[i].ingredients.join(
+          ",\n- ",
+        )}`);
+      }
+    }
+
+    return (recipe = `Sorry, ${name} does not exist on our list.`);
+  },
 };
+
+console.log(recipeMaker.giveRecipe("espresso"));
