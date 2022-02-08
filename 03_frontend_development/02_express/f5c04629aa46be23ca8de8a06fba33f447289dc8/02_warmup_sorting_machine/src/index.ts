7336576ad99f92b6f300c27ffb1b182519c0a559
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-08 10:08:12 +0100

    add and done warmup sorting machine

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/02_warmup_sorting_machine/src/index.ts
@@ -1,1 +1,9 @@
 // Code here
+function sortingMachine(arrayOfNumbers: number[], isTrue: boolean): number[] {
+  if (isTrue === true) {
+    return arrayOfNumbers.sort((numberOne, numberTwo) => numberOne - numberTwo);
+  } else {
+    return arrayOfNumbers.sort((numberOne, numberTwo) => numberTwo - numberOne);
+  }
+}
+export { sortingMachine };
