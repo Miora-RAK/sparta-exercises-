17fa45672d5204f33f0a6bf7e6d383a0fbf63312
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-08 09:56:22 +0100

    add and done exercise warmup iterate on data

--- 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/01_warmup_iterate_on_data/src/index.ts
+++ 03_frontend_development/02_express/f5c04629aa46be23ca8de8a06fba33f447289dc8/01_warmup_iterate_on_data/src/index.ts
@@ -1,1 +1,13 @@
 // Code here
+type CustomerType = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+};
+
+function getEmails(customerList: CustomerType[]): string[] {
+  return customerList.map((customer) => customer.email);
+}
+export { getEmails };
