fe8d0e4b8ee059d451c4f5c69f2122a34fc7a7d3
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-09 10:42:30 +0100

    add and done exercise fundamental capitalize context

--- 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
+++ 03_frontend_development/03_templating/941283fd0f53524acaead7d02ba27226e7dae5af/02_fundamental_capitalize_context/src/context.ts
@@ -1,5 +1,11 @@
-import { capitalize } from "./capitalize"
+import { capitalize } from "./capitalize";
 
 export function context(sentence: string): string {
   // Code here and use the function `capitalize` from exercise one
+  const sentenceToArray = sentence.split(" ");
+  for (let i = 0; i < sentenceToArray.length; i++) {
+    sentenceToArray[i] = capitalize(sentenceToArray[i]);
+  }
+  const sentenceCapitalized = sentenceToArray.join(" ");
+  return sentenceCapitalized;
 }
