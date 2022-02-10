3ed4106053d97cd522a918cdab505cd6b6c6cdab
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-10 10:56:43 +0100

    add and donne fundamentals antiVowels context

--- 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/context.ts
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/context.ts
@@ -1,7 +1,13 @@
-import { sentenceJoiner } from './sentenceJoiner';
-import { sentenceSplitter } from './sentenceSplitter';
+import { sentenceJoiner } from "./sentenceJoiner";
+import { sentenceSplitter } from "./sentenceSplitter";
 
-
-export const context = () => {
+export const context = (sentence: string): string => {
   // Code your function for context here
+
+  const arrayOfwords = sentenceSplitter(sentence);
+  console.log(arrayOfwords);
+  const arrayFiltered = arrayOfwords.filter((word) => !["e", "o", "i", "o", "u"].includes(word[0].toLowerCase()));
+  console.log(arrayFiltered);
+  const arrayFilteredJoined = sentenceJoiner(arrayFiltered);
+  return arrayFilteredJoined;
 };
--- 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/sentenceJoiner.ts
+++ 03_frontend_development/04_forms_and_cookies/e5b3d8438cc5eff9e68b205b3efb73619e89654c/02_fundamentals_antiVowels_context/src/sentenceJoiner.ts
@@ -1,1 +1,4 @@
 // Copy your function `sentenceJoiner` from the first exercise here
+export const sentenceJoiner = (wordsArray: string[]): string => {
+  return wordsArray.join(" ");
+};
