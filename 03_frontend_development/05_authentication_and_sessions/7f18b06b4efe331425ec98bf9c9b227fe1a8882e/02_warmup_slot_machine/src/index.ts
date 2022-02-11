af3fc21898614319f9934d0a6d2c820ce51c65b8
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-11 10:46:46 +0100

    add and done exercise warmup slot machine

--- 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/02_warmup_slot_machine/src/index.ts
+++ 03_frontend_development/05_authentication_and_sessions/7f18b06b4efe331425ec98bf9c9b227fe1a8882e/02_warmup_slot_machine/src/index.ts
@@ -1,1 +1,12 @@
 // Code here
+function slotMachine(myTuple: unknown[]): boolean {
+  // myTuple.forEach((element) => typeof element === "string")
+  if (
+    myTuple.every((element) => typeof element === "string") ||
+    myTuple.every((element) => typeof element === "number" || myTuple.every((element) => typeof element === "boolean"))
+  ) {
+    return myTuple.every((element) => element === myTuple[0]);
+  }
+  return myTuple.every((element) => element === myTuple[0]);
+}
+export { slotMachine };
