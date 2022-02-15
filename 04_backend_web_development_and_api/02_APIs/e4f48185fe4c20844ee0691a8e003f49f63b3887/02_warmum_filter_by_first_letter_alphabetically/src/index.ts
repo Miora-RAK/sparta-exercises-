b4d7deb4e01ed66c3a0b91a1bf1f98ef3f2c8bdc
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-16 00:50:51 +0100

    add and done exercise warmup filter by first letter alphabetically

--- 04_backend_web_development_and_api/02_APIs/e4f48185fe4c20844ee0691a8e003f49f63b3887/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
+++ 04_backend_web_development_and_api/02_APIs/e4f48185fe4c20844ee0691a8e003f49f63b3887/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
@@ -1,1 +1,18 @@
 // code here
+import { Customer } from "../data/customers";
+
+export function filterByFirstLetter(customersList: Customer[], letter: string): Customer[] {
+  return customersList.filter((customer) => customer.last_name.includes(letter));
+}
+
+export function filterByFirstLetterOrdered(customers: Customer[], letter: string): Customer[] {
+  return filterByFirstLetter(customers, letter).sort(function (customerA, customerB) {
+    if (customerA.last_name < customerB.last_name) {
+      return -1;
+    }
+    if (customerA.last_name > customerB.last_name) {
+      return 1;
+    }
+    return 0;
+  });
+}
