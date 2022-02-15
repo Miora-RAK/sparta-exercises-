9702b2515a4b21d05a667e01cf571bd49c861d60
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-15 23:19:11 +0100

    add and done exercise chuck norris

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getCategories.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getCategories.ts
@@ -1,7 +1,14 @@
 import request from "@fewlines-education/request";
 
-function getCategories() {
+function getCategories(): void {
   // code the function here
+  request("https://api.chucknorris.io/jokes/categories", (error, categories) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(categories);
+    }
+  });
 }
 
 // leave line below for tests to work properly
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
@@ -1,7 +1,14 @@
 import request from "@fewlines-education/request";
 
-function getChuckNorrisJoke() {
+function getChuckNorrisJoke(category: string): void {
   // code the function here
+  request(`https://api.chucknorris.io/jokes/random?category=${category}`, (error, oneJoke) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(JSON.parse(oneJoke));
+    }
+  });
 }
 
 // leave line below for tests to work properly
