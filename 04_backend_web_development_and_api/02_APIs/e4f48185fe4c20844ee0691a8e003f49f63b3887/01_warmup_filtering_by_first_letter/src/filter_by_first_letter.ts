e868bc182e8440b7168563d17a7c8dc6019cdd40
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-16 00:30:58 +0100

    add and done exercise warmup filtering by first letter

--- 04_backend_web_development_and_api/02_APIs/e4f48185fe4c20844ee0691a8e003f49f63b3887/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
+++ 04_backend_web_development_and_api/02_APIs/e4f48185fe4c20844ee0691a8e003f49f63b3887/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
@@ -1,5 +1,6 @@
 import { Customer } from "../data/customers";
 
-export function filterByFirstLetter() {
+export function filterByFirstLetter(customersList: Customer[], letter: string) {
   // Code here
+  return customersList.filter((customer) => customer.last_name.includes(letter));
 }
