427bf9e34d4ae2616a8c9ee5b0c13b4d150c53b6
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-15 20:54:11 +0100

    add and done exercise warmup loto checker

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/check.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/02_warmup_loto_checker/src/check.ts
@@ -2,15 +2,8 @@ import { lottery_draw } from "./lottery_draw";
 import { Draw } from "./types";
 
 export function checkIfIWon(ticket: Draw): void {
-  if (lottery_draw().length === ticket.length) {
-    console.log(
-      lottery_draw().every((element, index) => {
-        if (element === ticket[index]) {
-          console.log("You won the lottery!");
-        } else {
-          console.log("You lost...");
-        }
-      }),
-    );
-  }
+  console.log(lottery_draw());
+  lottery_draw().every((element, index) =>
+    element === ticket[index] ? console.log("You won the lottery!") : console.log("You lost..."),
+  );
 }
