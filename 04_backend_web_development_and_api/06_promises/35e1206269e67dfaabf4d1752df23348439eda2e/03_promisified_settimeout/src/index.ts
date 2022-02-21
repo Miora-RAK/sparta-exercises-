e3268033f6af9dffd212d59c20c6d5c01ba2f1e6
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-21 10:29:52 +0100

    add and done exercise promisified dettimeout

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
@@ -1,5 +1,10 @@
+import fetch from "node-fetch";
 function waitFor(time: number): Promise<void> {
-
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log(`This will be printed after ${time / 1000} seconds`));
+    }, time);
+  });
 }
 
 export { waitFor };
