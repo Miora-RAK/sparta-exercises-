daa9950b6a0011fd631141fbec687b2857c4f27c
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-21 11:25:45 +0100

    add and done exercise head or tail

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
@@ -1,7 +1,12 @@
 import { getRandomFace } from "./random-face";
 
-function headOrTail() {
-
+function headOrTail(headOrTail: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    if (getRandomFace() === headOrTail) {
+      resolve("Well done.");
+    } else {
+      reject("Nope.");
+    }
+  });
 }
-
 export { headOrTail };
