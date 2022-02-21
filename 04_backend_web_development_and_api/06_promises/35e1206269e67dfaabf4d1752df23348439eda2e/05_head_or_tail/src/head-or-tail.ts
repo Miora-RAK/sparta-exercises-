777310630e7cf6d327da530f8241e313836ec6ce
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-21 11:36:32 +0100

    modification

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/05_head_or_tail/src/head-or-tail.ts
@@ -1,8 +1,8 @@
 import { getRandomFace } from "./random-face";
 
-function headOrTail(headOrTail: string): Promise<string> {
+function headOrTail(userChoice: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    if (getRandomFace() === headOrTail) {
+    if (getRandomFace() === userChoice) {
       resolve("Well done.");
     } else {
       reject("Nope.");
