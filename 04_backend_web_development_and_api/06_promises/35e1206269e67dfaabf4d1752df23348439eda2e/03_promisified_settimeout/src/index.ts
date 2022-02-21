f265206c31ce62b2c196bee66c06a1887c9f2d66
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-21 11:37:58 +0100

    modification

--- 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
+++ 04_backend_web_development_and_api/06_promises/35e1206269e67dfaabf4d1752df23348439eda2e/03_promisified_settimeout/src/index.ts
@@ -1,6 +1,5 @@
-import fetch from "node-fetch";
 function waitFor(time: number): Promise<void> {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve(console.log(`This will be printed after ${time / 1000} seconds`));
     }, time);
