dafbdc488a1d8bc1a7d39411a31f1fabae70795a
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-21 09:53:05 +0100

    start openweather

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-city.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-city.ts
@@ -1,11 +1,20 @@
 import request from "@fewlines-education/request";
 import "dotenv/config";
 
-function weatherByCity(city) {
+function weatherByCity(city: string): void {
   // code the function here
+
+  request(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`,
+    (error, temperature) => {
+      if (error) {
+        console.error(error);
+      } else {
+        const json = JSON.parse(temperature);
+        console.log(`${json.main.temp} °C`);
+      }
+    },
+  );
 }
-
 // leave lines below for tests to work properly
-export { weatherByCity }
-
-
+export { weatherByCity };
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-forecast.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-forecast.ts
@@ -1,16 +1,57 @@
 import request from "@fewlines-education/request";
 import "dotenv/config";
 
-function weatherByZipcode(zipcode, countryCode) {
+function weatherByZipcode(zipcode: string, countryCode: string): void {
   // code the function here
+  request(
+    `http://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${countryCode}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`,
+    (error, weather) => {
+      if (error) {
+        console.error(error);
+      } else {
+        const json = JSON.parse(weather);
+        console.log(`Weather for ${json.city.name}`);
+        type Obj = {
+          date: string;
+          temperature: string;
+          weather: string;
+        };
+        const obj: Obj = {
+          date: json.list[0].dt_txt,
+          temperature: json.list[0].main.temp,
+          weather: json.list[0].weather[0].description,
+        };
+        console.log(obj);
+      }
+    },
+  );
 }
 
-function weatherByLatitudeAndLongitude(latitude, longitude) {
+function weatherByLatitudeAndLongitude(latitude: number, longitude: number): void {
   // code the function here
+  request(
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`,
+    (error, weathers) => {
+      if (error) {
+        console.error(error);
+      } else {
+        const hello = JSON.parse(weathers);
+        type Obj = {
+          date: string;
+          temperature: string;
+          weather: string;
+        };
+        const obj: Obj = {
+          date: hello.list[0].dt_txt,
+          temperature: hello.list[0].main.temp,
+          weather: hello.list[0].weather[0].description,
+        };
+        //date: ${json.list[0].dt_txt}\ntemperature : ${json.list[0].main.temp},\nweather : ${json.list[0].weather[0].main}
+        console.log(obj);
+      }
+    },
+  );
 }
 
 // leave lines below for tests to work properly
-export {
-  weatherByZipcode,
-  weatherByLatitudeAndLongitude
-}
+export { weatherByZipcode, weatherByLatitudeAndLongitude };
