743a3b1607dd99c6b257e7c5eba46a1653a7ca37
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-22 18:18:06 +0100

    add and done exercise github client

--- 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/03_github_client/src/github-client.ts
+++ 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/03_github_client/src/github-client.ts
@@ -4,32 +4,53 @@ export type Repo = {
   url: string;
   name: string;
   [key: string]: string | number | boolean | null;
-}
+};
 
 export type GitHub = {
   message: string;
   repos_url: string;
-}
+};
 
-
-export const getReposUrl = () => {
+export const getReposUrl = async (nickname: string): Promise<string> => {
   // Code here
-}
 
-export const getRepos = () => {
+  const repo = await fetch(`https://api.github.com/users/${nickname}`);
+  const user = await repo.json();
+  if (user.message === "Not Found") {
+    throw new Error("The User doesn't exist");
+  } else {
+    const result = await user.repos_url;
+    return result;
+  }
+};
+
+export const getRepos = async (url: string) => {
   // Code here
-}
+  const repo = await fetch(`${url}`);
+  const repoList = await repo.json();
+  console.log(repoList);
+  return repoList;
+};
 
-export const printRepos = () => {
+export const printRepos = (repoArray: Repo[]) => {
   // Code here
-}
+  for (let i = 0; i < repoArray.length; i++) {
+    console.log(`${i + 1} - ${repoArray[i].name}`);
+  }
+  return repoArray;
+};
 
-
-export const printRepository = () => {
+export const printRepository = (repository: Repo) => {
   // Code here
-}
+  console.log(
+    `name: ${repository.name}\ndescription:${repository.description}\nsubscribers:${repository.subscribers_count}\nstars:${repository.stargazers_count}\nlanguage:${repository.language}\nrepo_url:${repository.url}`,
+  );
+};
 
-export const getProjectInformations = () => {
+export const getProjectInformations = async (url: string) => {
   // Code here
-}
-
+  const repo = await fetch(`${url}`);
+  const repoData = await repo.json();
+  console.log(repoData);
+  return repoData;
+};
--- 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/03_github_client/src/index.ts
+++ 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/03_github_client/src/index.ts
@@ -1,16 +1,9 @@
-// import { getReposUrl, getRepos, printRepos, printRepository, getProjectInformations } from "../src/github-client";
+import { getReposUrl, getRepos, printRepos, printRepository, getProjectInformations } from "../src/github-client";
 
+getReposUrl("fewlines-education")
+  .then((url) => console.log(url))
+  .catch((error) => console.error(error));
 
-// getReposUrl("fewlines-education")
-//   .then(url => console.log(url))
-//   .catch(error => console.error(error))
+getRepos("https://api.github.com/users/fewlines-education/repos").then((repos) => printRepos(repos));
 
-
-
-// getRepos("https://api.github.com/users/fewlines-education/repos")
-//   .then(repos => printRepos(repos))
-
-
-
-// getProjectInformations("https://api.github.com/repos/fewlines-education/request")
-//   .then(repo => printRepository(repo))
+getProjectInformations("https://api.github.com/repos/fewlines-education/request").then((repo) => printRepository(repo));
