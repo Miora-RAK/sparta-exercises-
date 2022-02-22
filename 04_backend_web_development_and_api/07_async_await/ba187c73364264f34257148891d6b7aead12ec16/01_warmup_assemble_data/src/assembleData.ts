07476b56e38360a428fd941ae504b4e58db2a7e0
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-22 11:06:53 +0100

    add and done warmup assemble data

--- 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/01_warmup_assemble_data/src/assembleData.ts
+++ 04_backend_web_development_and_api/07_async_await/ba187c73364264f34257148891d6b7aead12ec16/01_warmup_assemble_data/src/assembleData.ts
@@ -5,6 +5,22 @@ type Profile = {
   haveTraveled: boolean;
 };
 
-export function assembleData() {
-  // Code here
+export function assembleData(
+  usersNames: string[],
+  usersAges: number[],
+  usersCountries: string[],
+  haveTraveled: boolean[],
+): Profile[] {
+  const array = [];
+  for (let i = 0; i < usersNames.length; i++) {
+    const obj: Profile = {
+      name: usersNames[i],
+      age: usersAges[i],
+      country: usersCountries[i],
+      haveTraveled: haveTraveled[i],
+    };
+    array.push(obj);
+  }
+
+  return array;
 }
