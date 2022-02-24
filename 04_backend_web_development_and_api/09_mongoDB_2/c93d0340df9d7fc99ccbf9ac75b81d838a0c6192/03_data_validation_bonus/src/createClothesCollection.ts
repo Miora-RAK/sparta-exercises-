deb389c170f7f947c5e336abdda4db02cf3fcd31
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-24 18:32:33 +0100

    add and done exercise bonus

--- 04_backend_web_development_and_api/09_mongoDB_2/c93d0340df9d7fc99ccbf9ac75b81d838a0c6192/03_data_validation_bonus/src/createClothesCollection.ts
+++ 04_backend_web_development_and_api/09_mongoDB_2/c93d0340df9d7fc99ccbf9ac75b81d838a0c6192/03_data_validation_bonus/src/createClothesCollection.ts
@@ -2,22 +2,131 @@ import * as mongo from "mongodb";
 
 export const sweaterProperties = {
   // write your sweater properties here
+  additionalProperties: false,
+  required: ["_id", "name", "color", "category", "size"],
+  properties: {
+    _id: {
+      bsonType: "objectId",
+    },
+    name: {
+      bsonType: "string",
+      maxLength: 30,
+      description: "must be a string < 30 characters and is required",
+    },
+    color: {
+      bsonType: "string",
+      maxLength: 15,
+      description: "must be a string < 15 characters and is required",
+    },
+    category: {
+      bsonType: "string",
+      enum: ["sweater"],
+      description: "must be only sweater",
+    },
+    size: {
+      bsonType: "string",
+      enum: ["XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+      description: "must be only a string in: `XXS`, `XS`, `S`, `M`, `L`, `XL`, `XXL`, or `XXXL`",
+    },
+  },
 };
 
 export const shoesProperties = {
   // write your shoes properties here
+  additionalProperties: false,
+  required: ["_id", "name", "color", "category", "size"],
+  properties: {
+    _id: {
+      bsonType: "objectId",
+    },
+    name: {
+      bsonType: "string",
+      maxLength: 30,
+      description: "must be a string < 30 characters and is required",
+    },
+    color: {
+      bsonType: "string",
+      maxLength: 15,
+      description: "must be a string < 15 characters and is required",
+    },
+    category: {
+      bsonType: "string",
+      enum: ["shoes"],
+      description: "must be only sweater",
+    },
+    size: {
+      bsonType: "int",
+      minimum: 30,
+      maximum: 50,
+      description: "must be **only** a `integer` between `30` and `50`",
+    },
+    material: {
+      bsonType: "string",
+      enum: ["leather", "textile", "synthetic"],
+      description: "must be **only** a string in `leather`, `textile`, or `synthetic` ",
+    },
+  },
 };
 
 export const pantsProperties = {
   // write your pants properties here
+  additionalProperties: false,
+  required: ["_id", "name", "color", "category", "size"],
+  properties: {
+    _id: {
+      bsonType: "objectId",
+    },
+    name: {
+      bsonType: "string",
+      maxLength: 30,
+      description: "must be a string < 30 characters and is required",
+    },
+    color: {
+      bsonType: "string",
+      maxLength: 15,
+      description: "must be a string < 15 characters and is required",
+    },
+    category: {
+      bsonType: "string",
+      enum: ["pants"],
+      description: "must be only sweater",
+    },
+    size: {
+      bsonType: "object",
+      additionalProperties: false,
+      required: ["width"],
+      properties: {
+        width: {
+          bsonType: "int",
+          minimum: 32,
+          maximum: 46,
+        },
+        cut: {
+          bsonType: "string",
+          enum: ["slim", "skinny", "regular", "straight"],
+        },
+
+        description: "must be only a string in: slim, skinny, regular, straight",
+      },
+    },
+  },
 };
 
 export const clothesValidator = {
   // write your validator here
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      additionalProperties: false,
+      oneOf: [
+        { bsonType: "object", sweaterProperties },
+        { bsonType: "object", pantsProperties },
+        { bsonType: "object", shoesProperties },
+      ],
+    },
+  },
 };
 
-export function createClothesCollection(
-  db: mongo.Db
-): Promise<mongo.Collection> {
+export function createClothesCollection(db: mongo.Db): Promise<mongo.Collection> {
   return db.createCollection("clothes", clothesValidator);
 }
--- 04_backend_web_development_and_api/09_mongoDB_2/c93d0340df9d7fc99ccbf9ac75b81d838a0c6192/03_data_validation_bonus/src/createMoviesCollection.ts
+++ 04_backend_web_development_and_api/09_mongoDB_2/c93d0340df9d7fc99ccbf9ac75b81d838a0c6192/03_data_validation_bonus/src/createMoviesCollection.ts
@@ -2,10 +2,52 @@ import * as mongo from "mongodb";
 
 export const moviesValidator = {
   // write your validator here
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      additionalProperties: false,
+      required: ["_id", "title", "genre", "year"],
+      properties: {
+        _id: {
+          bsonType: "objectId",
+        },
+        title: {
+          bsonType: "string",
+          description: "must be a string and is required",
+        },
+        genre: {
+          bsonType: "string",
+          enum: ["action", "comedy", "dramatic", "horror", "romance"],
+        },
+        year: {
+          bsonType: "int",
+          minimum: 1950,
+          maximum: 2020,
+          exclusiveMaximum: false,
+          description: "must be an integer in [ 1950, 2020 ]",
+        },
+        ratings: {
+          bsonType: "object",
+          additionalProperties: false,
+          required: ["pressRating", "spectatorsRating"],
+          properties: {
+            pressRating: {
+              bsonType: "int",
+              minimum: 1,
+              maximum: 5,
+            },
+            spectatorsRating: {
+              bsonType: "int",
+              minimum: 1,
+              maximum: 5,
+            },
+          },
+        },
+      },
+    },
+  },
 };
 
-export function createMoviesCollection(
-  db: mongo.Db
-): Promise<mongo.Collection> {
+export function createMoviesCollection(db: mongo.Db): Promise<mongo.Collection> {
   return db.createCollection("movies", moviesValidator);
 }
