66b8f3bc41e6d800bd720b61f61bc3361f52498f
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-03-14 20:25:58 +0100

    add and done exercise basic component

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/01_basic_component/src/components/HideOrNotHide.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/01_basic_component/src/components/HideOrNotHide.jsx
@@ -4,11 +4,11 @@ export const HideOrNotHide = () => {
   // Code here
   const hide = "Hide";
   const show = "Show";
-  const [buttonText, setButtonText] = React.useState("Show");
+  const [buttonText, setButtonText] = React.useState(show);
   const [showParagraph, setShowParagraph] = React.useState(true);
   const onClick = () => {
     setShowParagraph(!showParagraph);
-    setButtonText(buttonText === "Show" ? hide : show);
+    setButtonText(buttonText === show ? hide : show);
   };
   return (
     <div>
