0d872c9b7c519fbafe575f43d74a4675b4153c75
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-03-15 21:20:14 +0100

    add and done exercise forms and validations

--- 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
+++ 06_fullstack_web_development/01_react_functional_components/fe2008a224b48c8983a2d34ead9d860fe99af1ed/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [formText, setFormText] = React.useState("");
+  const [error, setError] = React.useState("");
+  const stringPatternValidation = (stringVal) => {
+    return /\s/g.test(stringVal);
+  };
+  const handleInputText = (event) => {
+    const value = event.target.value;
+    setFormText(event.target.value);
+    stringPatternValidation(value) ? setError("*no spaces allowed") : setError("");
+  };
+  // const handleFormInfos = (event) => {
+  //   return setFormText(event.target.value);
+  // };
+
+  return (
+    <form onSubmit={handleInputText}>
+      <div className="form-group">
+        <label htmlFor="firstName">First Name</label>
+        <input
+          onChange={handleInputText}
+          type="text"
+          className="form-control"
+          id="firstName"
+          placeholder="a space"
+          value={formText}
+        />
+        <p className="text-danger">{error && error}</p>
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Form;
