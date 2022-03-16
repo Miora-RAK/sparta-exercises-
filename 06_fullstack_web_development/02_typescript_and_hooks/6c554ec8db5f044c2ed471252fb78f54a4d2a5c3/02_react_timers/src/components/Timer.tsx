a855ed1e8a258ceb9ba4f0a23200c6bc0246f969
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-03-16 09:55:07 +0100

    start exercise react timers

--- 06_fullstack_web_development/02_typescript_and_hooks/6c554ec8db5f044c2ed471252fb78f54a4d2a5c3/02_react_timers/src/components/Timer.tsx
+++ 06_fullstack_web_development/02_typescript_and_hooks/6c554ec8db5f044c2ed471252fb78f54a4d2a5c3/02_react_timers/src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 type TimerProps = {
   timerId: string;
@@ -6,5 +6,33 @@ type TimerProps = {
 };
 
 export const Timer: React.FC<TimerProps> = ({ timerId, removeTimer }) => {
-  return <p>TIMER</p>;
+  // const [seconds, setSeconds] = React.useState(0);
+  // const [isActive, setIsActive] = React.useState(false);
+
+  // timerId = setIsActive(!isActive);
+
+  // removeTimer = (): void => {
+  //   setSeconds(0);
+  //   setIsActive(false);
+  // };
+
+  // React.useEffect(() => {
+  //   let interval = null;
+  //   if (isActive) {
+  //     interval = setInterval(() => {
+  //       setSeconds((seconds) => seconds + 1);
+  //     }, 1000);
+  //   } else if (!isActive && seconds !== 0) {
+  //     clearInterval(interval);
+  //   }
+  //   return (): void => clearInterval(interval);
+  // }, [isActive, seconds]);
+  // onClick={removeTimer}
+  return (
+    // <li className="list-group-item d-flex align-items-center  justify-content-between">
+    //   <p className="text-center">{seconds} seconds</p>
+    //   <div className="btn btn-danger">Delete</div>
+    // </li>
+    <p>TIMER</p>
+  );
 };
--- 06_fullstack_web_development/02_typescript_and_hooks/6c554ec8db5f044c2ed471252fb78f54a4d2a5c3/02_react_timers/src/components/TimersList.tsx
+++ 06_fullstack_web_development/02_typescript_and_hooks/6c554ec8db5f044c2ed471252fb78f54a4d2a5c3/02_react_timers/src/components/TimersList.tsx
@@ -2,7 +2,52 @@ import React from "react";
 import { Timer } from "./Timer";
 
 const TimersList: React.FC = () => {
-  return <p>TIMERS LIST</p>;
+  // const [timer, setTimer] = React.useState();
+
+  const [seconds, setSeconds] = React.useState(60);
+  const [isActive, setIsActive] = React.useState(false);
+
+  const toggle = (): void => {
+    setIsActive(!isActive);
+  };
+
+  const reset = (): void => {
+    console.log("Reset");
+    return setSeconds(0);
+  };
+  React.useEffect(() => {
+    // const addTimer = (): void => {
+    //   return setSeconds((seconds) => seconds + 1);
+    // };
+    return setSeconds((seconds) => seconds + 1);
+    // if (isActive) {
+    //   setInterval(() => {
+    //     setSeconds((seconds) => seconds + 1);
+    //   }, 1000);
+    // } else if (!isActive && seconds !== 0) {
+    //   clearInterval();
+    // }
+  }, []);
+  // const addTimer = (): void => {
+  //   console.log("add");
+  //   return setSeconds((seconds) => seconds + 1);
+  // };
+  return (
+    <ul className="list-group timers-list">
+      <li className="list-group-item text-center">
+        <button className="btn btn-primary" onClick={addTimer}>
+          Add a timer
+        </button>
+      </li>
+
+      <li className="list-group-item d-flex align-items-center  justify-content-between">
+        <p className="text-center">{seconds} seconds</p>
+        <div className="btn btn-danger" onClick={reset}>
+          Delete
+        </div>
+      </li>
+    </ul>
+  );
 };
 
 export default TimersList;
